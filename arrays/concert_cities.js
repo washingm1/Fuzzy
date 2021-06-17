f2903f4f0501191a3c8754dcfb5303a2537f8c0a
washingm1 <44791199+washingm1@users.noreply.github.com>
2021-06-17 09:44:31 -0400

    Add files via upload

---
+++ arrays/concert_cities.js
@@ -0,0 +1,40 @@
+const concerts =
+[
+ 
+    {roleName:'Dallas, TX', cmdName:'Dallas', id: '854794479256207380'},
+    {roleName:'Phoenix, AZ', cmdName:'Phoenix', id: '854794546780176404'},
+    {roleName:'San Diego, CA', cmdName:'SanDiego', id: '854794725831213076' },
+    {roleName:'Santa Ana, CA', cmdName:'SantaAna', id:'854794892824150036' },
+    {roleName:'Vancouver, BC', cmdName:'Vancouver', id:'854795033861685278' },
+    {roleName:'Salt Lake City, UT', cmdName:'SaltLake', id:'854795091767066625' },
+    {roleName:'Denver, CO', cmdName:'Denver', id: '854795151054864445' },
+    {roleName:'Indianapolis, IN', cmdName:'Indianapolis', id: '854795198390337556' },
+    {roleName:'Nashville, TN', cmdName:'Nashville', id: '854795446262169630' },
+    {roleName:'Atlanta, GA', cmdName:'Atlanta', id: '854795494416973855' },
+    {roleName:'Charlotte, NC', cmdName:'Charlotte', id: '854796777052045374' },
+    {roleName:'Philadelphia, PA', cmdName:'Philadelphia', id: '854796918001500160' },
+    {roleName:'Boston, MA', cmdName:'Boston', id: '854797060070440970' },
+    {roleName:'New York, NY', cmdName:'NewYork', id: '854797168615751711' },
+    {roleName:'Washington DC', cmdName:'DC', id: '854797217801043979' },
+    {roleName:'Columbus, OH', cmdName:'Columbus', id: '854797268653703200' },
+    {roleName:'St Louis, MO', cmdName:'StLouis', id: '854797342728912947' },
+    {roleName:'Cleveland, OH', cmdName:'Cleveland', id: '854797428418281482' },
+    {roleName:'Dublin, IE', cmdName:'Dublin', id: '854797532478963762' },
+    {roleName:'Glasgow, GBR', cmdName:'Glasgow', id: '854797641530736731' },
+    {roleName:'Leeds, GBR', cmdName:'Leeds', id: '854797697806499842' },
+    {roleName:'Manchester, GBR', cmdName:'Manchester', id: '854797756643541032' },
+    {roleName:'Birmingham, GBR', cmdName:'Birmingham', id: '854798536868757564' },
+    {roleName:'London, GBR', cmdName:'London', id: '854798605164216351' },
+    {roleName:'Brighton, GBR', cmdName:'Brighton', id: '854798757597544518' },
+    {roleName:'Bristol, GBR', cmdName:'Bristol', id: '854798814917033984' },
+    {roleName:'Paris, FR', cmdName:'Paris', id: '854798864972120064'}
+
+]
+
+module.exports = {concerts};
+
+
+module.exports.help = {
+    name:"concertCities",
+    aliases: [""]
+  }
