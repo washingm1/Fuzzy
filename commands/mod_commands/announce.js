5d576901bc71658a0b572ac067d35447903f55af
washingm1 <44791199+washingm1@users.noreply.github.com>
2021-07-26 11:06:33 -0400

    Added Admin only perms

--- commands/mod_commands/announce.js
+++ commands/mod_commands/announce.js
@@ -3,6 +3,10 @@ const db = require('quick.db')
 
 module.exports.run = async (bot, message, args) => {
     if(!message.content.startsWith('!'))return;  
+    
+   if (!message.member.hasPermission('ADMINISTRATOR')) {
+        return message.reply('You do not have enough permission to use this command.')
+    }
 
     const channel2 = bot.channels.cache.find(channel => channel.id === "659404619847303169");
 
