e2cadf70fdd6e94b0343a56ac9fdc72eda817394
washingm1 <44791199+washingm1@users.noreply.github.com>
2021-08-06 09:42:46 -0400

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,24 @@
+const client = require("../index");
+
+client.on("interactionCreate", async (interaction) => {
+    // Slash Command Handling
+    if (interaction.isCommand()) {
+
+        const cmd = client.slashCommands.get(interaction.commandName);
+        if (!cmd)
+            return await interaction.followUp({ content: "An error has occured " });
+
+        const args = [];
+
+        for (let option of interaction.options.data) {
+            if (option.type === "SUB_COMMAND") {
+                if (option.name) args.push(option.name);
+                option.options?.forEach((x) => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+
+        cmd.run(client, interaction, args);
+    }
+});
