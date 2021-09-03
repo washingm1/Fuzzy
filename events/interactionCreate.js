efcd503810e6780b7b342ac98ef9e1a2e907854c
washingm1 <44791199+washingm1@users.noreply.github.com>
2021-09-03 16:56:30 -0400

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,10 +3,9 @@ const client = require("../index");
 client.on("interactionCreate", async (interaction) => {
     // Slash Command Handling
     if (interaction.isCommand()) {
-
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd)
-            return await interaction.followUp({ content: "An error has occured " });
+            return interaction.followUp({ content: "An error has occured " });
 
         const args = [];
 
@@ -18,7 +17,17 @@ client.on("interactionCreate", async (interaction) => {
                 });
             } else if (option.value) args.push(option.value);
         }
+        interaction.member = interaction.guild.members.cache.get(interaction.user.id);
 
         cmd.run(client, interaction, args);
+
     }
+        // Context Menu Handling
+        if (interaction.isContextMenu()) {
+            await interaction.deferReply({ ephemeral: false });
+            const command = client.slashCommands.get(interaction.commandName);
+            if (command) command.run(client, interaction);
+        }
+
+      
 });
