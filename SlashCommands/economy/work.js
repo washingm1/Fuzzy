2bac48ee8a82861d103a83018a11829aada13082
washingm1 <44791199+washingm1@users.noreply.github.com>
2021-12-05 14:12:24 -0500

    Converted to Epoch timestamp

--- SlashCommands/economy/work.js
+++ SlashCommands/economy/work.js
@@ -5,7 +5,8 @@ const { MessageActionRow, MessageButton } = require('discord.js');
 const ms = require("parse-ms");
 const Canvas = require('canvas');
 const { MessageEmbed } = require('discord.js');
-
+const toMilliseconds = require('to-milliseconds');
+const now = new Date();
 
 module.exports = {
     name: "work",
@@ -23,23 +24,49 @@ module.exports = {
 run: async (client, interaction, args, message) => {
     console.log(interaction)
         await interaction.deferReply({ ephemeral: false }).catch(() => {});
+        
         let user = interaction.options.getUser('youruseroption') || interaction.user
         const guild = client.guilds.cache.get("646074330249429012");
         let author = await db.fetch(`work_${guild.id}_${user.id}`)
+        
+        let cmdChannel = interaction.guild.channels.cache.get("733043770454704189")
+        let returnEmbed = new MessageEmbed()
+        .setTitle('Oops!')
+        .setDescription(`Please use ${cmdChannel} for any bot commands`)
+    //    if (interaction.channel !== cmdChannel) return interaction.followUp({ embeds: [returnEmbed] });
 
     let timeout = 1800000;
     
     if (author !== null && timeout - (Date.now() - author) > 0) {
+
         let time = ms(timeout - (Date.now() - author));
-    
+
+        const timeInMs = toMilliseconds.convert(time)
+
+        const totalTime = now.valueOf() + timeInMs;
+       var printTime = totalTime.toString().slice(0,10)
+      
         let timeEmbed = new MessageEmbed()
         .setTitle('Try again Later')
         .setColor("#6051e8")
-        .setDescription(`🚫You have already worked recently\n\nTry again in ${time.minutes}m ${time.seconds}s `);
+        .setDescription(`🚫You have already worked recently\n\nTry again in <t:${printTime}:R>`);
         interaction.editReply({embeds: [timeEmbed] })
       } else {
 
-        let replies = ['Cashier','Uber Driver','Crash Test Dummy','Telemarketer','Waiter','Doordash Driver', 'producer on FL Studio', 'Mafia Boss','Car salesman', 'Bedroom Pop artist' ]
+        let replies = ['a Cashier',
+        'an Uber Driver',
+        'a Crash Test Dummy',
+        'a Telemarketer',
+        'a Waiter',
+        'a Doordash Driver', 
+        'a producer on FL Studio',
+         'a Mafia Boss',
+        'a Car salesman',
+         'a Bedroom Pop artist',
+        'a Dog Walker',
+        'a Youtuber',
+      'a Twitch Streamer',
+      'a Tree salesperson']
 
         let result = Math.floor((Math.random() * replies.length));
         let amount = Math.floor(Math.random() * 80) + 1;
@@ -47,8 +74,11 @@ run: async (client, interaction, args, message) => {
         let embed1 = new MessageEmbed()
         .setColor("#6051e8")
         .setTitle('Good Work!')
-        .setDescription(`:white_check_mark: You worked as a ${replies[result]} and earned ${amount} coins`);
-        interaction.editReply({ embeds: [embed1]})
+        .setDescription(`:white_check_mark: You worked as ${replies[result]} and earned ${amount} coins`);
+
+        let workMsg = interaction.editReply({ embeds: [embed1]})
+
+        if (amount === 69) workMsg.react(':verynice:885370854647664681');
         
         db.add(`money_${guild.id}_${user.id}`, amount)
         db.set(`work_${guild.id}_${user.id}`, Date.now())
@@ -58,11 +88,15 @@ run: async (client, interaction, args, message) => {
         let workUses = db.fetch(`working_${guild.id}_${user.id}`)
 
     
-        if (workUses === 50){
-            interaction.channel.send(`You have used daily ${workUses} times, and have earned the `)
-          }
+  
           if (workUses === 100){
-            interaction.channel.send(`You have used daily ${workUses} times, and have earned the `)
+            interaction.channel.send(`You have used daily ${workUses} times, and have earned the \`HARD WORKER\` badge!`)
+          }
+          if (workUses === 250){
+            interaction.channel.send(`You have used daily ${workUses} times, and have earned the \`WORK HORSE\` badge!`)
+          }
+          if (workUses === 500){
+            interaction.channel.send(`You have used daily ${workUses} times, and have earned the \`WORK MACHINE\` badge!`)
           }
     };
 },
